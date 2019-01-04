583b4da8b76ce55daaaa87300a44f1a210198907
chen <chen@DESKTOP-2RJI49B>
2019-01-04 14:39:38 +0800

    第一次提交

---
+++ fatesky/src/main/java/com/fatesky/FateskyApplication.java
@@ -0,0 +1,14 @@
+package com.fatesky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FateskyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FateskyApplication.class, args);
+	}
+
+}
+
